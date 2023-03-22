7428cedd550a0af57891c9b17afe950fd7e02126
Zainab <zainabjafari998@gmail.com>
2023-03-22 14:37:48 +0100

    script added

---
+++ script.js
@@ -0,0 +1,20 @@
+          const menubtn = document.getElementById("menubtn")
+          const menu = document.getElementById("menu")
+          const sideNav = document.getElementById("sideNav")
+
+          sideNav.style.right = "-250px"
+          menubtn.onclick = function () {
+            if (sideNav.style.right == "-250px") {
+              sideNav.style.right = "0"
+              menu.src = "bilder/close.png";
+            }
+            else {
+              sideNav.style.right = "-250px"
+              menu.src = "bilder/menu2.png";
+            }
+            var scroll = new SmoothScroll('a[href*="#"]', {
+              speed: 500,
+              speedAsDuration: true
+            });
+          }
+        
